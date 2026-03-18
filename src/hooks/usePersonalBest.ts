41410c266e40db6fef83f456c9d74b5ff8ebe6e2
Thalita Silva <thalitasilva687@gmail.com>
2026-03-18 20:08:34 -0300

    "feat: add o hook usePersonalBest com persistência no localStorage"

---
+++ src/hooks/usePersonalBest.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import type{ PersonalBest } from "../types";
+
+export function usePersonalBest() {
+    const [personalBest, setPersonalBest] = useState<PersonalBest | null>(null);
+
+    useEffect(() => {
+        const stored = localStorage.getItem(STORAGE_KEY)
+
+        if (stored) {
+            const parsed: PersonalBest = JSON.parse(stored);
+
+            setPersonalBest(parsed);
+        }
+    }, [])
+
+    const savePersonalBest = (wpm: number) => {
+        const newBest: PersonalBest = { wpm }
+
+        setPersonalBest(newBest);
+
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(newBest))
+    }
+
+    return { personalBest, savePersonalBest }
+}
